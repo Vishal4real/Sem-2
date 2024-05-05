517114ee4dfa34d602d9eeb8b46dd15d943ce8fe
Vishal <leo3369706@gmail.com>
2024-05-05 23:41:33 +0530

    All the best

--- Fsd/test.js
+++ Fsd/test.js
@@ -1,51 +1,62 @@
-var http = require('http')
-var fs = require('fs')
-var querystring = require('querystring')
-var port = 3002;
+const mysql = require('mysql');
 
-http.createServer(function (req, res) {
-    let body = '';
-    if (req.method === 'GET') {
-        fs.readFile('index.html', function (err, data) {
-            if (err) {
-                res.writeHead(404, { 'Content-Type': 'text/html' });
-                res.write("File Not Found");
-                return res.end();
-            }
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(data);
-            return res.end();
-        });
-    } else if (req.method === 'POST') {
-        req.on('data', chunk => {
-            body += chunk.toString();
-        });
-
-        req.on('end', () => {
-            const postData = querystring.parse(body);
-            const file_1 = postData['file1'];
-            const file_2 = postData['file2'];
-
-            fs.readFile(file_1, 'utf-8', (err, data) => {
-                if (err) {
-                    res.writeHead(404, { 'Content-Type': 'text/html' });
-                    res.write("File Not Found");
-                    return res.end();
-                }
-
-                fs.appendFile(file_2, data, 'utf-8', err => {
-                    if (err) {
-                        res.writeHead(500, { 'Content-Type': 'text/html' });
-                        res.write("Error Appending File");
-                        return res.end();
-                    }
-                    res.writeHead(200, { 'Content-Type': 'text/html' });
-                    res.write("File Appended");
-                    return res.end();
-                });
-            });
-        });
-    }
-}).listen(port, () => {
-    console.log(`Server running at port ${port}`);
+const con = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'Vishal@22'
 });
+
+con.connect((err)=>{
+    if(err) throw err;
+    console.log("Connected to mysql");
+});
+
+const createdb = "create database customer;";
+
+con.query(createdb,(err)=>{
+    if(err) throw err;
+    console.log("Created Customer database succesfuly.");
+});
+
+con.changeUser({
+    database:'customer'
+});
+
+const createtb = "create table cust_details(cus_id int primary key, cus_name varchar(40));";
+
+con.query(createtb, (err)=>{
+    if(err) throw err;
+    console.log("Table created successfuly.");
+})
+
+const inserttb = [
+    [1,"Vishal"],
+    [2,"Tejas"],
+    [3,"Shivam"],
+    [4,"Sourabh"],
+    [5,"Riya"]
+];
+
+const insertq = "insert into cust_details values ?;";
+
+con.query(insertq,[inserttb],(err)=>{
+    if(!err) console.log("Values inserted succesfuly");
+});
+
+const showdet = "select * from cust_details;"
+
+con.query(showdet,(err,result)=>{
+    if(!err) console.log(result);
+});
+
+const showdetcus = "select * from cust_details where cus_name like 'S%';";
+
+con.query(showdetcus,(err,result)=>{
+    if(err) throw err;
+    console.log("Showing customised output:");
+    result.forEach(ele=>{
+        console.log(`Customer Id : ${ele.cus_id} Customer name : ${ele.cus_name}`);
+    });
+});
+
+con.end();
