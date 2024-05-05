9db503d958f3d2e00c890ad222403555d9964ca8
Vishal <leo3369706@gmail.com>
2024-05-05 11:43:58 +0530

    afbaj

--- Mt/Database/DbHelper.java
+++ Mt/Database/DbHelper.java
@@ -1,4 +1,4 @@
-package com.example.databasedemo;
+package com.example.demodata;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
@@ -8,29 +8,31 @@ import androidx.annotation.Nullable;
 
 public class DbHelper extends SQLiteOpenHelper {
 
-    static final String DATABASE_NAME = "MY_COMPANY.DB";
-    static final int DATABASE_VERSION  = 1;
+    static final String DATABASE_NAME = "STUDENTS.DB";
+    static final int VERSION = 1;
 
-    static final String DATABASE_TABLE = "USER";
-    static final String USER_ID = "id";
-    static final String USER_NAME = "username";
-    static final String USER_PASSWORD = "password";
-    static final String CREATE_TABLE = "CREATE TABLE "+DATABASE_TABLE+" ( "
-            +USER_ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "
-            +USER_NAME+" TEXT NOT NULL, "
-            +USER_PASSWORD+" TEXT NOT NULL);";
+    static final String TABLE_NAME = "STUDENT";
 
-    public DbHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    static final String ROLL_NO = "roll_no";
+    static final String STUD_NAME = "stud_name";
+    static final String STUD_AGE = "stud_age";
+    static final String STUD_CLASS = "stud_class";
+
+    static final String CREATE_TABLE = "CREATE TABLE "+ TABLE_NAME + "( "
+            +ROLL_NO+ " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            +STUD_NAME+ " TEXT NOT NULL,"
+            +STUD_AGE+ " INTEGER NOT NULL,"
+            +STUD_CLASS+ " TEXT NOT NULL);";
+    public DbHelper( Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_TABLE);
     }
-
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS "+DATABASE_TABLE);
+        db.execSQL("DROP TABLE IF EXISTS "+ TABLE_NAME);
     }
 }
--- Mt/Database/Mainactivity.java
+++ Mt/Database/Mainactivity.java
@@ -1,57 +1,80 @@
-package com.example.databasedemo;
+package com.example.demodata;
 
 import static java.lang.Integer.parseInt;
+
 import android.annotation.SuppressLint;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.sql.SQLClientInfoException;
 import java.sql.SQLDataException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText username, password, id;
-    DbManger dbManger;
+    EditText stud_name,stud_roll,stud_age,stud_class;
+    Button insert,update,delete,view;
+    DbManager dbManager;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        stud_age = findViewById(R.id.age);
+        stud_class =findViewById(R.id.stud_class);
+        stud_name = findViewById(R.id.name);
+        stud_roll = findViewById(R.id.rollno);
+        insert = findViewById(R.id.insert);
+        update = findViewById(R.id.update);
+        delete = findViewById(R.id.delete);
+        view = findViewById(R.id.view);
 
-        username = findViewById(R.id.editTextText);
-        password = findViewById(R.id.editTextTextPassword);
-        id = findViewById(R.id.editTextNumber);
+        insert.setOnClickListener(this);
+        update.setOnClickListener(this);delete.setOnClickListener(this);
+        view.setOnClickListener(this);
 
-        dbManger = new DbManger(this);
+        dbManager = new DbManager(this);
+        dbManager.open();
 
-        try {
-            dbManger.open();
-        } catch (SQLDataException e) {
-            throw new RuntimeException(e);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.insert){
+            dbManager.insert(stud_name.getText().toString(),parseInt(stud_age.getText().toString()),stud_class.getText().toString());
+            Log.i("STUD_DATABASE","INSERTED");
         }
-    }
-
-    public void btnInsertedPressed(View v){
-        dbManger.insert(username.getText().toString(),password.getText().toString());
-    }
-    public void btnUpdatedPressed(View v){
-        dbManger.update(parseInt(id.getText().toString()),username.getText().toString(),password.getText().toString());
-    }
-    public void btnFetchPressed(View v){
-        Cursor cursor = dbManger.fetch();
-        if(cursor!=null){
-            do{
-                @SuppressLint("Range") String username = cursor.getString(cursor.getColumnIndex(DbHelper.USER_NAME));
-                @SuppressLint("Range") String id = cursor.getString(cursor.getColumnIndex(DbHelper.USER_ID));
-                @SuppressLint("Range") String password = cursor.getString(cursor.getColumnIndex(DbHelper.USER_PASSWORD));
-                Log.i("DATABASE",id+" "+username+" "+" "+password);
-            }while(cursor.moveToNext());
+        if(v.getId()==R.id.delete){
+            dbManager.delete(parseInt(stud_roll.getText().toString()));
+            Log.i("STUD_DATABASE","DELETED");
         }
-    }
-    public void btnDeletePressed(View v){
-        dbManger.delete(parseInt(id.getText().toString()));
+        if(v.getId()==R.id.update){
+            dbManager.update(parseInt(stud_roll.getText().toString()),stud_name.getText().toString(),parseInt(stud_age.getText().toString()),stud_class.getText().toString());
+            Log.i("STUD_DATABASE","UPDATED");
+        }
+
+        if(v.getId()==R.id.view){
+            Cursor cursor =  dbManager.fetch();
+            if(cursor!=null || cursor.moveToFirst()){
+                do{
+                    @SuppressLint("Range") String roll = cursor.getString(cursor.getColumnIndex(DbHelper.ROLL_NO));
+                    @SuppressLint("Range") String name = cursor.getString(cursor.getColumnIndex(DbHelper.STUD_NAME));
+                    Log.i("STUD_DATABASE",roll+" "+ name);
+                }while (cursor.moveToNext());
+            }
+            cursor.close();
+        }
     }
 }
